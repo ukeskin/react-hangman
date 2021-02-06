49c74fd4727a1cbfea039cdba022ebd1ca245979
ukeskin <ukeskin26262@gmail.com>
2021-02-06 17:08:29 +0300

    🧹

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <>
+            <h1>Hangman</h1>
+            <p>Find the hidden word - Enter a letter</p>
+        </>
+    )
+}
